a34bfe4790d0a67d3314597708751744e67361eb
Pial <pialname394@gmail.com>
2026-05-01 16:41:41 +0600

    create products route

---
+++ src/app/products/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductsDetailsPage = () => {
+  return (
+    <div>
+      <h2>This is products Details Page</h2>
+    </div>
+  );
+};
+
+export default ProductsDetailsPage;
