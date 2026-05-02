13ee5bd613d825308c64c67dd13a5cbecc6d661f
Pial <pialname394@gmail.com>
2026-05-02 16:50:26 +0600

    added some classes for responsiveness

--- src/app/(main)/products/page.jsx
+++ src/app/(main)/products/page.jsx
@@ -4,8 +4,8 @@ import { getAllProducts } from "@/lib/fetchData";
 const ProductsPage = async () => {
   const allProducts = await getAllProducts();
   return (
-    <div className="container mx-auto my-10">
-      <div className="grid grid-cols-3 gap-5">
+    <div className="container mx-auto my-10 px-2 md:px-0">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {allProducts.map((product) => {
           return <CardPage key={product.id} product={product} />;
         })}
