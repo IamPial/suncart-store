e4bc5d7632136b2ea78b3c63a9f84cd51054e107
Pial <pialname394@gmail.com>
2026-05-02 15:18:41 +0600

    render summer care tips components here

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/shared/homepage/Banner";
 import PopularProductsPage from "@/components/shared/homepage/popularProducts";
+import SummerCarePage from "@/components/shared/homepage/Summer";
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <PopularProductsPage />
+      <SummerCarePage />
     </div>
   );
 }
