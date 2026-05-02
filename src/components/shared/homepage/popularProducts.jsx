bb20cfffadf85f0d623f955b17fbc50a134abdac
Pial <pialname394@gmail.com>
2026-05-02 15:17:17 +0600

    added some responsive design classes

--- src/components/shared/homepage/popularProducts.jsx
+++ src/components/shared/homepage/popularProducts.jsx
@@ -1,15 +1,12 @@
 import { getAllProducts } from "@/lib/fetchData";
-
-import React from "react";
 import CardPage from "../Card";
 
 const PopularProductsPage = async () => {
   const products = await getAllProducts();
-  console.log(products);
   return (
-    <div className="container mx-auto my-10">
+    <div className="container mx-auto my-10 px-2 md:px-0">
       <h2 className="text-3xl font-bold text-slate-800">Popular Products</h2>
-      <div className="grid grid-cols-3 gap-5 mt-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
         {products.slice(0, 3).map((p) => {
           return <CardPage key={p.id} product={p} />;
         })}
