4c54e9ebaf084335ab5d024088791cf16788d734
Pial <pialname394@gmail.com>
2026-05-02 12:52:59 +0600

    render the popurlar products components here

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/shared/homepage/Banner";
+import PopularProductsPage from "@/components/shared/homepage/popularProducts";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <PopularProductsPage />
     </div>
   );
 }
